5446a71ef877a2659bf15b64f60f2519838d5d3c
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-08-21 19:29:05 -0500

    first

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    let first = `<div>first</div>`;
+    let second = `<div>second</div>`;
+    $('.container')
+        .append(first)
+        .append(second);
+});
